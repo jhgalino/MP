a8b39a622bced9a66a3a704675f55c1ecf099f1a
John Henry Galino <jagalino@up.edu.ph>
2020-06-13 18:52:07 +0800

    Added comments in MPD

--- include/MPD.py
+++ include/MPD.py
@@ -1,19 +1,19 @@
 def top_and_bottom(li: list, n: int):
     returnList = []
-    if checkLists(li) is False:
-        while len(li) >= n * 2:
+    if checkLists(li) is False:  # check if there are no lists inside list
+        while len(li) >= n * 2:  # n*2 to account for the back
             for i in range(n):
                 returnList.append(li[0])
-                li.pop(0)
+                li.pop(0)  # pop start
             for i in range(n):
                 returnList.append(li[-1])
-                li.pop()
+                li.pop()  # pop end
         returnList.append(li)
     else:
         while len(li) >= n * 2:
             for i in range(n):
                 if type(li[i]) == list:
-                    li[i] = top_and_bottom(li[i])
+                    li[i] = top_and_bottom(li[i])  # recurse if type is list
     print(li)
     print(returnList)
 
