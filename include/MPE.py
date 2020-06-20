556748cd4eef6edf7adf2735f008833509d6e994
John Henry Galino <jagalino@up.edu.ph>
2020-06-20 13:39:54 +0800

    TODO: add logic for calculation of differential

--- include/MPE.py
+++ include/MPE.py
@@ -3,3 +3,34 @@ def differentiate(fxn: str):
     hasExp = False
     hasTrig = False
     diff = ""
+
+    # separate string into three parts which containe exponent, coeff,
+    # and the function inside the parentheses
+    def splitByParentheses(function: str):
+        function = function.replace("(", "~", 1)
+        function = function[::-1].replace(")", "~", 1)
+        function = function[::-1].split("~")
+        return function
+
+    tempFxn = splitByParentheses(fxn)
+
+    if len(tempFxn) < 3:  # if x is the only then left, omit x
+        return
+
+    if tempFxn[0] != "" and tempFxn[0].isdecimal():
+        hasCoeff = True
+    elif tempFxn[0] != "" and tempFxn[0].isalpha():
+        hasTrig = True
+
+    if tempFxn[-1] != "" and tempFxn[-1][-1].isdecimal():
+        hasExp = True
+
+    # if fxn is (x)
+    if tempFxn[1] == "x" and tempFxn[0] == "" and tempFxn[2] == "":
+        return 1
+
+    if hasExp and hasCoeff:  # change coeff and exp into ints, then calculate
+        tempFxn[0] = int(tempFxn[0])
+        tempFxn[2] = int(tempFxn[2][1])
+        tempFxn[0] = tempFxn[0] * tempFxn[2]
+        tempFxn[2] -= 1
